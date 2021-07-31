2b7dd5f833d4ff663d211d312d105d523457d9ae
abhiru-wije <abdwije19972219@gmail.com>
2021-07-31 22:24:38 +0530

    First git update

---
+++ first git.R
@@ -0,0 +1,29 @@
+View(cars)
+my_age <- 12
+your_age <- 14
+sum(my_age, your_age)
+plot(cars)
+hist(cars$dist)
+summary(cars)
+summary(cars$speed)
+cars[1:6, 1]
+cars[1:6, 1:2]
+new_data <- c(1, 2, 3, 5, 6, 9, NA, 16, NaN)
+mean(new_data)
+mean(new_data, na.rm = TRUE)
+sum(new_data)
+sum(new_data, na.rm = TRUE)
+sum(new_data)
+sum(new_data, na.rm = TRUE)
+d <- seq(1,5, by = 2)
+d
+d <- seq(1,10, by = 2)
+d
+qty <- c(1,2,3,4,5)
+price <- c(10,11,12,13,14)
+func1 <- 2 * qty * price
+func1
+a <- 1:5
+b <- 4:7
+intersect(a, b)
+union(a, b)
